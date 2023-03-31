ab86d1cc92462ec63903fd02f413f2a7f7eefb03
Prakash <98002602+PrakashThangasamy@users.noreply.github.com>
2023-03-31 11:03:33 +0530

    Delete task6 directory

--- task6/script.js
+++
@@ -1,6 +0,0 @@
-  let boxes = document.querySelectorAll('.flex-item');
-    window.addEventListener('scroll',function(){
-    for (let i = 0; i < boxes.length; i++) {
-       boxes[i].classList.add("flex-item");
-    }
-})
